6a988200ba89cef9c534ddc3e860d2e7a9800fd0
Jamshaid Alam <jamshaidalam88@gmail.com>
2018-11-15 00:54:20 +0530

    Sellcraft Task

---
+++ Task 1/Assets/Scripts/PathVisualiser.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PathVisualiser : MonoBehaviour {
+
+    public NavMeshAgent agent;
+    LineRenderer lineRenderer;
+    // Use this for initialization
+    void Start () {
+        lineRenderer = GetComponent<LineRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(agent.hasPath)
+        {
+            lineRenderer.positionCount = agent.path.corners.Length;
+            lineRenderer.SetPositions(agent.path.corners);
+            lineRenderer.enabled = true;
+        } else
+        {
+            lineRenderer.enabled = false;
+        }
+		
+	}
+}
---
+++ Task 1/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerScript : MonoBehaviour {
+
+    public Camera cam;
+    public NavMeshAgent NMAgent;
+    Ray ray;
+    RaycastHit hit;
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray,out hit))
+            {
+                NMAgent.SetDestination(hit.point);
+            }
+        }
+		
+	}
+}
